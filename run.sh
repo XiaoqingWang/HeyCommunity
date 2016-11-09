0566d6f9fc23a97ecc4064d34c69852afb2dc99b
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-10 02:52:00 +0800

    the app

--- run.sh
+++ run.sh
@@ -4,10 +4,3 @@ echo '################'
 pwd
 cd /app/backend
 pwd
-
-if [ -n "$MYSQL_PORT_3306_TCP" ]; then
-    echo 'run migrate'
-    # php artisan migrate:refresh --seed
-else
-    echo 'not run migrate'
-fi
