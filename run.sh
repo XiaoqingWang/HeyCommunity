3fe40b23db27ee2464d35241158fce98c1d24c74
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-13 11:55:56 +0800

    the dockerfile

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '################'
+pwd
+cd /app/backend
+pwd
+
+if [ -n "$MYSQL_PORT_3306_TCP" ]; then
+    echo 'run migrate'
+    php artisan migrate:refresh --seed
+else
+    echo 'not run migrate'
+fi
+
+## apache2 foreground
+apache2-foreground
