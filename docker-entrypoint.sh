a72fc1ab330f4fe5661e0ed63e251bad68a574ba
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-13 22:00:19 +0800

    test docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 
-echo '################################';
-env
 
-if [ -n "$MYSQL_PORT_3306_TCP" ]; then
-    echo 'run migrate'
-    php artisan migrate:refresh --seed
+##
+##
+echo '################################';
+
+if [ -n "$MYSQL_PORT_3306_TCP_PORT" ]; then
+
+  dbVal="
+    \n\n
+    export DB_HOST=$MYSQL_PORT_3306_TCP_ADDR\n
+    export DB_PORT=$MYSQL_PORT_3306_TCP_PORT\n
+  ";
+
+  if [ -n "$MYSQL_INSTANCE_NAME" ]; then
+    dbVal=$dbVal"
+      export DB_DATABASE=$MYSQL_INSTANCE_NAME\n
+      export DB_USERNAME=$MYSQL_USERNAME\n
+      export DB_PASSWORD=$MYSQL_PASSWORD\n
+    "
+  else
+    dbVal=$dbVal"
+      export DB_DATABASE=$MYSQL_ENV_MYSQL_DATABASE\n
+      export DB_USERNAME=root\n
+      export DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD\n
+    "
+  fi
+
+  echo -e $dbVal
+  echo -e $dbVal >> ~/.bashrc
+  source ~/.bashrc
+
+  php /app/backend/artisan migrate:refresh --seed
+
+  echo 'With the database and perform the migration'
 else
-    echo 'not run migrate'
+  echo 'There is no database'
 fi
 
 
+##
+##
+echo '### debug'
+env
+
+
+##
 ## apache2 foreground
 apache2-foreground
