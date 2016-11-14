08c23d6555ce35b97496b10ea99f380dddf3f0f0
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-14 12:12:02 +0800

    test docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,6 +20,14 @@ if [ -n "$MYSQL_PORT_3306_TCP_PORT" ]; then
     export DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
   fi
 
+  echo -e "\n\n" >> /etc/profile
+  echo "export DB_HOST=$DB_HOST" >> /etc/profile
+  echo "export DB_PORT=$DB_PORT" >> /etc/profile
+  echo "export DB_DATABASE=$DB_DATABASE" >> /etc/profile
+  echo "export DB_USERNAME=$DB_USERNAME" >> /etc/profile
+  echo "export DB_PASSWORD=$DB_PASSWORD" >> /etc/profile
+  source /etc/profile
+
   php artisan migrate:refresh --seed \
   && echo 'With the database and perform the migration' \
   || echo 'An error occurred'
