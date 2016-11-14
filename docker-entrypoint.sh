df3f64e495a331e97c613eb4c2d0939fc02f3bd4
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-14 11:19:14 +0800

    test docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,34 +3,24 @@
 
 ##
 ##
-env
-echo -e "\n\n\n\n ### Deploy"
+echo -e "\n\n\n\n### Deploy"
 
 if [ -n "$MYSQL_PORT_3306_TCP_PORT" ]; then
 
-  dbVal="
-    \n\n
-    export DB_HOST=$MYSQL_PORT_3306_TCP_ADDR\n
-    export DB_PORT=$MYSQL_PORT_3306_TCP_PORT\n
-  ";
+  export DB_HOST=$MYSQL_PORT_3306_TCP_ADDR
+  export DB_PORT=$MYSQL_PORT_3306_TCP_PORT
 
   if [ -n "$MYSQL_INSTANCE_NAME" ]; then
-    dbVal=$dbVal"
-      export DB_DATABASE=$MYSQL_INSTANCE_NAME\n
-      export DB_USERNAME=$MYSQL_USERNAME\n
-      export DB_PASSWORD=$MYSQL_PASSWORD\n
-    "
+    export DB_DATABASE=$MYSQL_INSTANCE_NAME
+    export DB_USERNAME=$MYSQL_USERNAME
+    export DB_PASSWORD=$MYSQL_PASSWORD
   else
-    dbVal=$dbVal"
-      export DB_DATABASE=$MYSQL_ENV_MYSQL_DATABASE\n
-      export DB_USERNAME=root\n
-      export DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD\n
-    "
+    export DB_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
+    export DB_USERNAME=root
+    export DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
   fi
 
-  echo -e $dbVal >> ~/.bashrc \
-  && source ~/.bashrc \
-  && php /app/backend/artisan migrate:refresh --seed \
+  php artisan migrate:refresh --seed \
   && echo 'With the database and perform the migration' \
   || echo 'An error occurred'
 else
@@ -40,12 +30,10 @@ fi
 
 ##
 ##
-echo -e "\n\n\n\n ### Debug"
+echo -e "\n\n\n\n### Debug"
 env
 
 
 ##
 ## apache2 foreground
 apache2-foreground
-
-echo -e "\n\n ****** HeyCommunityApp deploy successfuly"
